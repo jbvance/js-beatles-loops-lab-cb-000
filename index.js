ce11a9a91b7cd412c0a38e23032a751360028ba8
Jason Vance <jbvance@gmail.com>
2017-12-21 14:26:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var theBeatlesPlay = (musicians, instruments) => {
-  
-}
+  facts = [];
+  for (let i = 0; i < musicians.length; i++) {
+    facts.push(`${musician[i]} plays ${instrument[i]} `)
+  }
+  return facts;
+};
